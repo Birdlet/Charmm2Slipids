8e4228c25b4d45908fda4ed482089b658d626aca
Birdlet <dk_dingkang@126.com>
2026-01-16 10:51:47 +0800

    update lipid conditions
    Fix typo in variable name and update conditions for 'CHOL'.

--- charmm2slipid.py
+++ charmm2slipid.py
@@ -94,6 +94,7 @@ def convert(fin, fout):
 			if tmp[11:15] == "  O3":
 				new_line[i-1] =  tmp[0:5] + 'CHOL' + tmp[9:]
 				new_line[i-1] = _tmp[0:5] + 'CHOL' + tmp[9:]
+				continue
 			new_line[i] = tmp[0:5] + 'CHOL' + tmp[9:]
 			
 		elif tmp[5:9].strip() == 'TIP3':
